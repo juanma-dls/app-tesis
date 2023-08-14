193e3ff816261805fb528050732b18e5a390b716
Juanma De los Santos <juanmadls6@gmail.com>
2023-08-14 15:48:42 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
